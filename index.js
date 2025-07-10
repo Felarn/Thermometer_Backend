7c35d98a487b268d544bb606fb787b3457966e96
Felarn <120136549+Felarn@users.noreply.github.com>
2025-07-10 16:45:24 +0000

    ADD basic HTTP server

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(`\n[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+
+  let body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        if (body) console.log('Body:', body);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Request logged');
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
